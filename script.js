6bea9073d0e5cfccbd3b34b1376e8dccacef11b1
Антонина Максимова <tonkastar@mail.ru>
2020-10-31 23:27:28 +0300

    no message

---
+++ script.js
@@ -0,0 +1,42 @@
+let easy = document.getElementById('easy');
+let medium = document.getElementById('medium');
+let difficult = document.getElementById('difficult');
+let button = document.getElementById ('button');
+let playingCard = document.querySelectorAll('.card');
+
+easy.addEventListener ('click', () => {
+	easy.classList.add('active');
+	medium.classList.remove('active');
+	difficult.classList.remove('active');
+})
+
+medium.addEventListener ('click', () => {
+	medium.classList.add('active');
+	easy.classList.remove('active');
+	difficult.classList.remove('active');
+})
+
+difficult.addEventListener ('click', () => {
+	difficult.classList.add('active');
+	easy.classList.remove('active');
+	medium.classList.remove('active');
+})
+
+button.addEventListener ('click', () => {
+	document.querySelector('.container').classList.toggle('disable');
+	let start = document.querySelector('.active');
+
+	switch(true) {
+		case start == document.getElementById('medium'):
+		document.querySelector('.container-medium').classList.remove('disable');
+		break;
+		case start == document.getElementById('difficult'):
+		document.querySelector('.container-difficult').classList.remove('disable');
+		break;
+		default:
+		document.querySelector('.container-easy').classList.remove('disable');
+	}
+})
+
+const cardOnClick = () => playingCard.classList.add('card__change');
+playingCard.addEventListener('click', cardOnClick);
